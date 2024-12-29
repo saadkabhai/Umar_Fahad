df1f9b1d8769b724a1468063559e39bb8cdc2c8e
Saad <ujkmuhj@gmail.com>
2024-12-29 19:12:48 +0500

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Sidebarcomponent from '@/component/sidebar'
-import Main from '@/component/main'
+'use client'
+import dynamic from 'next/dynamic';
 
-export default function page() {
+const Sidebar = dynamic(() => import('@/component/sidebar'), {
+  ssr: false, // This will prevent SSR and only render it on the client side
+});
+
+const Main = dynamic(() => import('@/component/main'), {
+  ssr: false, // This will prevent SSR and only render it on the client side
+});
+
+export default function Page() {
   return (
-    <div style={{display:'flex'}}>
-      <Sidebarcomponent />
-      <Main/>
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
+      <Main />
     </div>
-  )
+  );
 }
