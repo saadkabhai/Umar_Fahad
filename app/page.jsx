8bc0a11ce888ad5d94d045e5831885078e007512
Saadmajid <128070661+saadkabhai@users.noreply.github.com>
2024-12-29 19:14:52 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,11 @@
-'use client'
-import dynamic from 'next/dynamic';
-
-const Sidebar = dynamic(() => import('@/component/sidebar'), {
-  ssr: false, // This will prevent SSR and only render it on the client side
-});
-
-const Main = dynamic(() => import('@/component/main'), {
-  ssr: false, // This will prevent SSR and only render it on the client side
-});
+import Main from "@/component/main";
+import Sidebarcomponent from "@/component/sidebar";
+import React from "react";
 
 export default function Page() {
   return (
     <div style={{ display: 'flex' }}>
-      <Sidebar />
+      <Sidebarcomponent />
       <Main />
     </div>
   );
